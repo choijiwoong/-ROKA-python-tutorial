a05ebf52e315ca88042fdc65ccd4f6b198181ff1
choijiwoong <gogogi313@naver.com>
2022-02-14 19:37:23 +0900

    numpy, pandas, matplotlib에 대한 간단한 조작, Async 예제 주석 완료

--- 핵심만 간단히, Hello World!/14. Async.py
+++ 핵심만 간단히, Hello World!/14. Async.py
@@ -8,10 +8,11 @@ s=time.time()
 results=[]
 
 #--part A--
-async def getpage(url):
-    req=await loop.run_in_executor(None, requests.get, url)
+async def getpage(url):#실제 페이지를 불러오는 코루틴
+    req=await loop.run_in_executor(None, requests.get, url)#awit은 코루틴을 실행하는 것이며, 실행한 코루틴은 loop.run_in_executor()로
+    #코루틴만 실행되는 파이썬환경에서 일반 함수를 코루틴처럼 실행시켜주는 역활을 한다. 즉 그것만 다른거지 위는 requests.get(url)이다. 첫인자는 executor라고 한다.
     html=req.text
-    soup=await loop.run_in_executor(None, BeautifulSoup, html, 'lxml')
+    soup=await loop.run_in_executor(None, BeautifulSoup, html, 'lxml')#마찬가지로 BeautifulSoup(html('lxml'))을 코루틴화하여 실행한다
     return soup
 #--part B--
 async def main():
@@ -24,9 +25,9 @@ async def main():
             "https://wp.me/p9x2W1-h"]#크롤링 리스트들
 
     fts=[asyncio.ensure_future(getpage(u)) for u in urls]#asyncio의 future오브젝트를 만들어 fts에 넣고, 내용은 urls의 page들이다.
-    r=await asyncio.gather(*fts)
-    global results
-    results=r
+    r=await asyncio.gather(*fts)#future(계획서)를 실행하고, 결과를 r에 담는다. C++로 따지면 asyncio.gather(*fts)로 futures를 gather하고 결과 promise를 담는다.(
+    global results#글로벌 변수를 선언하고
+    results=r#결과를 담는다.
 #--part C--
 loop=asyncio.get_event_loop()#루프 생성
 loop.run_until_complete(main())#main종료시까지 루프 작동

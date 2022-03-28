aaac1ec88224f516a0c734ef9c8a327341355ef5
choijiwoong <gogogi313@naver.com>
2022-03-28 19:09:48 +0900

    모델 완료해서 컴파일 결과 확인했고, 기억해야할 것은 메모리 네크워트의 구조일듯. story가 2개로 각각 임베딩 되어 a는 embedded question과 dot softmax로 유사도 구하고 이를 b에 add. 그 후 질문을 강조하기 위해 embedded question과 add결과를 concat하고 LSTM-Dense-Softmax통과시킨다.

--- 딥 러닝을 이용한 자연어 처리/21. 질의 응답(Question Answering)/1. 메모리 네트워크(Memory Network, MemN)를 이용한 QA.py
+++ 딥 러닝을 이용한 자연어 처리/21. 질의 응답(Question Answering)/1. 메모리 네트워크(Memory Network, MemN)를 이용한 QA.py
@@ -199,13 +199,13 @@ input_encoded_m=input_encoder_m(input_sequence)#입력값 임베딩층 통과
 input_encoded_c=input_encoder_c(input_sequence)
 question_encoded=question_encoder(question)
 print('Input encoded m', input_encoded_m)
-print('Input encoded c', input_encoded_c)
+print('**Input encoded c**', input_encoded_c)
 print('Question encoded', question_encoded)
 
 #스토리 단어, 질문 단어 유사도 계산
 match=dot([input_encoded_m, question_encoded], axes=-1, normalize=False)
 match=Activation('softmax')(match)
-print("Match shape", match)#(sampled, story_maxlen, question_max_len)? embedding_dim아닌가
+print("**Match shape**", match)#(sampled, story_maxlen, question_max_len)? embedding_dim아닌가
 
 response=add([match, input_encoded_c])#(samples, story_max_len, question_max_len)
 response=Permute((2,1))(response)#(samples, question_max_len, story_max_len)
@@ -222,3 +222,30 @@ answer=Activation('softmax')(answer)
 model=Model([input_sequence, question], answer)
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
 print(model.summary())
+
+history=model.fit([Xstrain, Xqtrain],
+                  Ytrain, batch_size, train_epochs,
+                  validation_data=([Xstest, Xqtest], Ytest))
+model.save('model.h5')
+print('테스트 정확도: ', model.evaluate([Xstest, Xqtest], Ytest)[1])
+
+#가시화
+plt.subplot(211)
+plt.title('Accuracy')
+plt.plot(history.history['acc'], color='g', label='train')
+plt.plot(history.history['val_acc'], color='b', label='validation')
+plt.legend(loc='best')
+
+plt.subplot(212)
+plt.title('Loss')
+plt.plot(history.history['loss'], color='g', label='train')
+plt.plot(history.history['val_loss'], color='b', label='validation')
+plt.legend(loc='best')
+
+plt.tight_layout()
+plt.show()
+
+ytest=np.argmax(Ytest, axis=1)
+
+Ytest_=model.predict([Xstest, Xqtest])
+ytest_=np.argmax(Ytest_, axis=1)
